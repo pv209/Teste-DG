bf4e6169035e11fb6a6c80b173603dd9bc190f2e
Paulo Vitor de Farias Borges <pvtrybe209@gmail.com>
2022-06-07 17:15:30 -0300

    Delete src directory

--- src/routes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const UserController = require('./Controller/UserController.js')
-router.post('/cadastro', UserController.cadastro);
-router.get('/listar', UserController.listar);
-
- module.exports = router;
